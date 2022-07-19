231e5cb95549c00653f7342f05dfe72267b70cab
Mantou-9487 <opcantel@gmail.com>
2022-07-19 13:27:04 +0800

    Push Github

--- cogs/info.py
+++ cogs/info.py
@@ -12,14 +12,16 @@ class info(Cog_Extension):
     @commands.command(aliases=['info'])
     async def _info(self, ctx):
         guild = self.bot.get_guild(889054851496046632)
-        emoji = discord.utils.get(guild.emojis, name="python")
+        python_emoji = discord.utils.get(guild.emojis, name="python")
+        linux_emoji = discord.utils.get(guild.emojis, name="linux")
+        heroku_emoji = discord.utils.get(guild.emojis, name="heroku")
         embed = discord.Embed()
         embed = discord.Embed(title="鰻頭機器人#2692", description="", color=discord.Colour.random(), timestamp= datetime.datetime.utcnow())
         embed = discord.Embed(color=discord.Colour.random(), title="機器人狀態 :robot:", inline=True) 
         embed.set_author(name=self.bot.user.name, icon_url=str(self.bot.user.avatar_url))
-        embed.add_field(name="系統型號", value="{}".format(str(platform.system() + platform.release()), inline=False))
-        embed.add_field(name=f"{emoji}" + "託管商", value="Heroku", inline=True)
-        embed.set_footer(text="本機器人使用Discord.py烹煮而成 (？")
+        embed.add_field(name=f"{linux_emoji}"+"系統型號", value="{}".format(str(platform.system() + platform.release()), inline=False))
+        embed.add_field(name=f"{heroku_emoji}"+"託管商", value="Heroku", inline=True)
+        embed.set_footer(text=f"{python_emoji}" + " 本機器人使用Discord.py烹煮而成 (？")
         await ctx.channel.send(embed=embed)
 
 def setup(bot):
