aa7a7e8bc39f69f04bed31870c1168007f75673c
Mantou-9487 <opcantel@gmail.com>
2022-07-21 21:11:28 +0800

    Push Github

--- cogs/luck.py
+++ cogs/luck.py
@@ -17,7 +17,8 @@ class luck(Cog_Extension):
         embed = discord.Embed(color=discord.Colour.random(), title="運勢唬爛器", description="", timestamp= datetime.datetime.utcnow()) 
         embed.set_author(name=self.bot.user.name, icon_url=str(self.bot.user.avatar_url))
         embed.add_field(name=f"今日運勢" , value="||{}||".format(f'{l}'), inline=True)
-        await ctx.channel.send(f'ctx.author.mention', embed=embed)
+        a = embed=embed
+        await ctx.channel.send(f'{ctx.author.mention}' + "\n" + a)
     
    
    
